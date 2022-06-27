7720b0f73c3f949bde5c970a043a9fbcda3f5d28
NataliiaChapai <natalya.chapay@gmail.com>
2022-06-27 23:52:19 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store.store}>
       <React.StrictMode>
         <PersistGate loading={<Spinner animation="border" variant="primary" />} persistor={store.persistor}>
-          <BrowserRouter>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </PersistGate>
